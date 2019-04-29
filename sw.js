ec87d9c5a66bc43d245b7f42debe5d355b520e94
bayron412 <bayron412@hotmail.com>
2019-04-29 17:34:49 -0500

    ios

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const IMMUTABLE_CACHE = 'immutable-v1';
 
